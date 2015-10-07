e49e2ef7d299b3a52aac33dfdfa2fe78b2bcfd10
Martin Gausby <martin@gausby.dk>
2015-10-07 11:32:45 +0200

    added a simple plug that serve static data

--- mix.exs
+++ mix.exs
@@ -4,29 +4,20 @@ defmodule Torrent.Mixfile do
   def project do
     [app: :torrent,
      version: "0.0.1",
-     elixir: "~> 1.1-rc",
+     elixir: "~> 1.1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
   # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :cowboy, :plug]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  # Dependencies
   defp deps do
-    []
+    [{:cowboy, "~> 1.0.0"},
+     {:plug, "~> 1.0"}]
   end
 end
