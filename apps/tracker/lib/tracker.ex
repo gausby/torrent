6df67f388e7978c00ed3e96c09d71c78d30e1c60
Martin Gausby <martin@gausby.dk>
2015-10-08 11:49:45 +0200

    Added a scrape endpoint
      - For now it only serves dummy data (the data the user passed in)
      - It has a custom qs parser, as the default one will overwrite
        info_hashes if more than one is given

--- apps/tracker/lib/tracker.ex
+++ apps/tracker/lib/tracker.ex
@@ -10,15 +10,42 @@ defmodule Tracker do
     Plug.Adapters.Cowboy.shutdown __MODULE__.HTTP
   end
 
-  plug Plug.Logger
   plug :match
   plug :dispatch
 
   @data "hello, world!"
-
   get "announce" do
     send_resp conn, 200, Bencode.encode(@data)
   end
+
+  get "scrape" do
+    case get_info_hashes(conn) do
+      [] ->
+        send_resp conn, 200, Bencode.encode("all")
+
+      info_hashes ->
+        send_resp conn, 200, Bencode.encode(info_hashes)
+    end
+  end
+
+  # Creating a custom query string parser specificly for info_hash-values,
+  # as the default query parser will return a map, resulting in only the
+  # last given info_hash being send as it is overwritten everytime it sees
+  # a info_hash in the query_string.
+  #
+  # this is a problem because some clients will ask for stuff like:
+  #   /scrape?info_hash=foo&info_hash=bar
+  defp get_info_hashes(%Plug.Conn{} = conn) do
+    conn.query_string
+    |> String.split("&")
+    |> Enum.filter_map(&info_hash?/1, &extract_info_hash/1)
+  end
+
+  defp info_hash?(topic),
+    do: String.starts_with? topic, "info_hash="
+
+  defp extract_info_hash(<<"info_hash=", info_hash::binary>>),
+    do: info_hash
 
   match _ do
     send_resp conn, 404, "File not found"
--- apps/tracker/test/tracker_test.exs
+++ apps/tracker/test/tracker_test.exs
@@ -6,8 +6,25 @@ defmodule TrackerTest do
   defp call(mod, conn),
     do: mod.call(conn, [])
 
+  # dummy data for now!
+
   test "getting announce" do
     conn = call(Tracker, conn(:get, "announce"))
     assert conn.resp_body == Bencode.encode("hello, world!")
+  end
+
+  test "should be able to scrape" do
+    conn = call(Tracker, conn(:get, "scrape"))
+    assert conn.resp_body == Bencode.encode("all")
+  end
+
+  test "should be able to specify the info_hash of interest when scraping" do
+    conn = call(Tracker, conn(:get, "scrape?info_hash=aaaaaaaaaaaaaaaaaaaa"))
+    assert conn.resp_body == Bencode.encode(["aaaaaaaaaaaaaaaaaaaa"])
+  end
+
+  test "should be able to specify multiple info_hashes of interest when scraping" do
+    conn = call(Tracker, conn(:get, "scrape?info_hash=aaaaaaaaaaaaaaaaaaaa&info_hash=bbbbbbbbbbbbbbbbbbbb&info_hash=cccccccccccccccccccc"))
+    assert conn.resp_body == Bencode.encode(["aaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbb", "cccccccccccccccccccc"])
   end
 end
