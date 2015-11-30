8d7c76c9068259904c09cd5aa57413fc81d3def7
Martin Gausby <martin@gausby.dk>
2015-12-01 00:46:21 +0100

    Removed some cruft

--- apps/tracker/lib/tracker.ex
+++ apps/tracker/lib/tracker.ex
@@ -18,21 +18,3 @@ defmodule Tracker do
     supervise(children, strategy: :simple_one_for_one)
   end
 end
-
-# defmodule Tracker do
-#   use Application
-#   require Logger
-
-#   def start(_type, _args) do
-#     import Supervisor.Spec, warn: false
-
-#     children = [
-#       supervisor(Tracker.Torrent.Supervisor, []),
-#       supervisor(Tracker.Peer.Supervisor, []),
-#     ]
-
-#     opts = [strategy: :one_for_one, name: Tracker.Supervisor]
-#     Logger.info "Starting #{__MODULE__}"
-#     Supervisor.start_link(children, opts)
-#   end
-# end
