78f6065b5a00f578c60a752121b23d83cc44395e
Martin Gausby <martin@gausby.dk>
2015-12-23 20:59:40 +0100

    Added the beginning of a bitfield implementation

---
+++ apps/bitfield/lib/bitfield.ex
@@ -0,0 +1,78 @@
+defmodule Bitfield do
+  defstruct(
+    size: 0,
+    pieces: MapSet.new
+  )
+
+  @pad [0, 0, 0, 0, 0, 0, 0 ,0]
+  defp pad(digits) do
+    Enum.take(@pad, 8 - length digits) ++ digits
+  end
+
+  defp number_to_padded_bits(n) when is_number(n) do
+    Integer.digits(n, 2) |> pad
+  end
+
+  # ====================================================================
+  def new(size) do
+    %__MODULE__{size: size}
+  end
+
+  def new(size, content) when is_binary(content) do
+    pieces = reduce_bits(content, fn
+      {index, 1}, acc ->
+        [index|acc]
+      _, acc ->
+        acc
+    end)
+    %__MODULE__{size: size, pieces: MapSet.new(pieces)}
+  end
+
+  @doc """
+  Reduce the bits in the bytes in the bit-field
+  """
+  def reduce_bits(bytes, fun),
+    do: do_reduce_bits(bytes, 0, [], fun)
+
+  def do_reduce_bits(<<>>, _index, acc, _fun),
+    do: Enum.reverse acc
+  def do_reduce_bits(<<byte::integer, rest::binary>>, index, acc, fun) do
+    {index, acc} =
+      number_to_padded_bits(byte)
+      |> Enum.reduce({index, acc}, fn bit, {index, acc} ->
+           {index + 1, fun.({index, bit}, acc)}
+         end)
+
+    do_reduce_bits(rest, index, acc, fun)
+  end
+
+  def set(%__MODULE__{pieces: pieces, size: size} = state, piece)
+  when is_number(piece) and piece < size do
+    %__MODULE__{state|pieces: MapSet.put(pieces, piece)}
+  end
+
+  def member?(%__MODULE__{pieces: pieces, size: size}, piece_number) when piece_number < size do
+    MapSet.member?(pieces, piece_number)
+  end
+
+  def intersection(%__MODULE__{size: size, pieces: a}, %__MODULE__{size: size, pieces: b}) do
+    %__MODULE__{size: size, pieces: MapSet.intersection(a, b)}
+  end
+
+  def to_binary(%__MODULE__{size: size, pieces: pieces}) when size > 0 do
+    have = MapSet.to_list(pieces)
+    bit_range = 0..(size - 1)
+
+    Stream.transform(bit_range, have, fn
+      # is the same
+      i, [i|rest] ->
+        {[1], rest}
+      # is not the same
+      _, rest ->
+        {[0], rest}
+    end)
+    |> Stream.chunk(8)
+    |> Enum.map(&(Integer.undigits(&1, 2)))
+    |> IO.iodata_to_binary
+  end
+end
---
+++ apps/bitfield/test/bitfield_test.exs
@@ -0,0 +1,78 @@
+defmodule BitfieldTest do
+  use ExUnit.Case
+  doctest Bitfield
+
+  test "creating a new bitfield" do
+    bitfield = Bitfield.new(32)
+    expected = MapSet.new
+    assert %Bitfield{size: 32, pieces: ^expected} = bitfield
+  end
+
+  test "creating a new bitfield with data" do
+    bitfield = Bitfield.new(32, <<128, 64, 32, 48>>)
+
+    expected = MapSet.new([0, 9, 18, 26, 27])
+    assert %Bitfield{size: 32, pieces: ^expected} = bitfield
+  end
+
+  test "turning a bitfield into a binary" do
+    result =
+      Bitfield.new(24, <<74, 0, 0>>)
+      |> Bitfield.to_binary
+    expected = <<74, 0, 0>>
+    assert result == expected
+
+    result =
+      Bitfield.new(24, <<0, 74, 0>>)
+      |> Bitfield.to_binary
+    expected = <<0, 74, 0>>
+    assert result == expected
+
+    result =
+      Bitfield.new(24, <<0, 0, 74>>)
+      |> Bitfield.to_binary
+    expected = <<0, 0, 74>>
+    assert result == expected
+
+    result =
+      Bitfield.new(24, <<1, 255, 74>>)
+      |> Bitfield.to_binary
+    expected = <<1, 255, 74>>
+    assert result == expected
+  end
+
+  test "getting bits" do
+    bitfield = Bitfield.new(32, <<128, 129, 255, 1>>)
+
+    assert Bitfield.member?(bitfield, 0) == true
+    assert Bitfield.member?(bitfield, 1) == false
+    assert Bitfield.member?(bitfield, 8) == true
+    assert Bitfield.member?(bitfield, 14) == false
+    assert Bitfield.member?(bitfield, 15) == true
+    assert Bitfield.member?(bitfield, 16) == true
+
+    assert Bitfield.member?(bitfield, 30) == false
+    assert Bitfield.member?(bitfield, 31) == true
+  end
+
+  test "setting bits" do
+    result =
+      Bitfield.new(16)
+      |> Bitfield.set(2)
+      |> Bitfield.set(4)
+      |> Bitfield.set(6)
+      |> Bitfield.set(8)
+      |> Bitfield.set(15)
+
+    expected = <<42, 129>>
+    assert Bitfield.to_binary(result) == expected
+  end
+
+  test "intersection" do
+    bitfield1 = Bitfield.new(16, <<190, 106>>)
+    bitfield2 = Bitfield.new(16, <<106, 190>>)
+
+    expected = Bitfield.new(16, <<42, 42>>)
+    assert expected == Bitfield.intersection(bitfield1, bitfield2)
+  end
+end
