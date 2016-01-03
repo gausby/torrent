a7a29be046768685eb875c0ff30cf9b81e7f6612
Martin Gausby <martin@gausby.dk>
2016-01-03 22:37:21 +0100

    A receiver will split incoming data into packages
      - a receiver will listen for length messages and then start consuming
        the given amount of bytes; this results in a package.
      - When a package has been consumed it will get emitted (for now: to
        the stdout)
      - Missing: tests and a package handler.
    
    I took some inspiration on how to do this from the peerwire
    implementation in etorrent.

--- lib/torrent/file/swarm/peer/receiver.ex
+++ lib/torrent/file/swarm/peer/receiver.ex
@@ -4,11 +4,14 @@ defmodule Torrent.File.Swarm.Peer.Receiver do
   alias :gen_tcp, as: TCP
 
   defstruct(
-    socket: nil
+    state: :awaiting_socket,
+    socket: nil,
+    buffer: [],
+    remaining: nil
   )
   alias Torrent.File.Swarm.Peer.Receiver, as: State
 
-  # Client API
+  #=Client API =========================================================
   def start_link(info_hash, {ip, port}) do
     GenServer.start_link(__MODULE__, %State{socket: nil}, name: via_name(info_hash, ip, port))
   end
@@ -24,31 +27,80 @@ defmodule Torrent.File.Swarm.Peer.Receiver do
       receiver_pid = :gproc.where(peer_name(info_hash, ip, port)),
       :ok <- GenServer.call(receiver_pid, {:set_socket, socket}),
       :ok <- TCP.controlling_process(socket, receiver_pid),
-      :ok <- set_readable(socket),
+      :ok <- :inet.setopts(socket, active: :once),
       do: :ok
     )
   end
 
-  defp set_readable(socket),
-    do: :inet.setopts(socket, active: :once)
-
-  # Server callbacks
+  #=Server callbacks ===================================================
   def init(state) do
     {:ok, state}
   end
 
-  def handle_call({:set_socket, socket}, _from, state) do
-    {:reply, :ok, %State{state|socket: socket}}
-  end
-
-  def handle_info({:tcp, _port, message}, %State{socket: socket} = state) do
-    IO.inspect PeerWire.decode_message(message)
-    set_readable(socket)
-    {:noreply, state}
+  def handle_call({:set_socket, socket}, _from, %State{state: :awaiting_socket} = state) do
+    {:reply, :ok, %State{state|socket: socket, state: :ready}}
   end
 
   def handle_info({:tcp_closed, _port}, state) do
-    IO.inspect "closing"
     {:stop, :normal, state}
   end
+
+  def handle_info({:tcp, socket, inbound}, state) do
+    new_state = handle_inbound(inbound, state)
+
+    :inet.setopts(socket, active: :once)
+    {:noreply, new_state}
+  end
+
+  #=helpers ============================================================
+  defp handle_inbound(inbound, state) do
+    case do_handle_inbound(inbound, state) do
+      {:scanning_for_length, data} ->
+        %State{state|state: :scanning_for_length, buffer: data, remaining: nil}
+
+      {:consuming_data, remaining, data_acc} ->
+        %State{state|state: :consuming_data, buffer: data_acc, remaining: remaining}
+
+      {:emit, package, remaining_data} ->
+        # todo, send packages on to a handler process instead of stdout
+        IO.inspect {:emit, package}
+        handle_inbound(remaining_data, %{state|state: :ready, buffer: [], remaining: nil})
+
+      :reset ->
+        %State{state|state: :ready, buffer: [], remaining: nil}
+    end
+  end
+
+  defp do_handle_inbound(<<>>, %State{state: :ready}),
+    do: :reset
+  # emit "alive"
+  defp do_handle_inbound(<<0::big-integer-size(32), remaining::binary>>, %State{state: :ready}),
+    do: {:emit, <<>>, remaining}
+  # start consuming data
+  defp do_handle_inbound(<<len::big-integer-size(32), remaining::binary>>, %State{state: :ready} = state),
+    do: do_handle_inbound(remaining, %State{state|state: :consuming_data, remaining: len, buffer: []})
+
+  # scanning for length ------------------------------------------------
+  defp do_handle_inbound(inbound_data, %State{state: :ready})
+  when byte_size(inbound_data) < 4,
+    do: {:scanning_for_length, inbound_data}
+
+  defp do_handle_inbound(<<inbound::binary>>, %State{state: :scanning_for_length, buffer: data} = state),
+    # retry to see if we have enough data now
+    do: do_handle_inbound(<<data::binary, inbound::binary>>, %State{state|state: :ready, buffer: []})
+
+  # consuming data -----------------------------------------------------
+  defp do_handle_inbound(inbound, %State{state: :consuming_data, remaining: remaining, buffer: data_acc})
+  when byte_size(inbound) >= remaining do
+    <<data::binary-size(remaining), remaining_data::binary>> = inbound
+
+    package =
+      [data|data_acc]
+      |> Enum.reverse
+      |> IO.iodata_to_binary
+
+    {:emit, package, remaining_data}
+  end
+  defp do_handle_inbound(inbound, %State{state: :consuming_data, remaining: remaining, buffer: data_acc}),
+    do: {:consuming_data, remaining - byte_size(inbound), [inbound | data_acc]}
 end
