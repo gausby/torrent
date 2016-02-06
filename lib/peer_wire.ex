9eec5fbd7f3fb9537cfc3850168ee5b44d8813ce
Martin Gausby <martin@gausby.dk>
2016-02-06 13:15:09 +0100

    Got rid of an unnecessary with expression

--- lib/peer_wire.ex
+++ lib/peer_wire.ex
@@ -11,15 +11,13 @@ defmodule PeerWire do
   end
 
   def receive_handshake(socket) do
-    with(
-      capabilities = local_capabilities(),
-      initial_handshake = protocol_header(capabilities),
-      :ok <- TCP.send(socket, initial_handshake),
-      {:ok, <<@protocol_length, @protocol, _capabilities::binary-size(8),
-              info_hash::binary-size(20), peer_id::binary-size(20)>>} <- TCP.recv(socket, 68, 5000),
+    capabilities = local_capabilities()
+    initial_handshake = protocol_header(capabilities)
+    :ok = TCP.send(socket, initial_handshake)
+    {:ok, <<@protocol_length, @protocol, _capabilities::binary-size(8),
+            info_hash::binary-size(20), peer_id::binary-size(20)>>} = TCP.recv(socket, 68, 5000)
 
-      do: {:ok, info_hash, peer_id}
-    )
+    {:ok, info_hash, peer_id}
   end
 
   def complete_handshake(socket, info_hash, peer_id) do
