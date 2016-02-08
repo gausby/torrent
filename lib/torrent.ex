59da2dae1ca3df4767b891f2c865c4f2fb9dc44c
Martin Gausby <martin@gausby.dk>
2016-02-09 00:09:35 +0100

    No need to round the result from random generator

--- lib/torrent.ex
+++ lib/torrent.ex
@@ -6,7 +6,7 @@ defmodule Torrent do
   end
 
   def generate_peer_id() do
-    random_number_stream = Stream.repeatedly(fn -> round(:rand.uniform * 9) end)
+    random_number_stream = Stream.repeatedly(fn -> :rand.uniform(10) - 1 end)
     header = "-EX0001-"
     IO.iodata_to_binary [header, Enum.take(random_number_stream, 20 - byte_size header)]
   end
