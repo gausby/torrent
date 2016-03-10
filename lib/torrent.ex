01d3399f1a117cd56fec6cd8f646bf368cef0570
Martin Gausby <martin@gausby.dk>
2016-03-09 21:39:44 -0800

    Reordered the functions in the main file

--- lib/torrent.ex
+++ lib/torrent.ex
@@ -11,14 +11,14 @@ defmodule Torrent do
     |> Torrent.Supervisor.start_link
   end
 
+  def add(<<"d", _::binary>> = raw) do
+    {:ok, data, info_hash} = Bencode.decode_with_info_hash(raw)
+    Torrent.Processes.add(info_hash, data)
+  end
+
   def generate_peer_id() do
     random_number_stream = Stream.repeatedly(fn -> :rand.uniform(10) - 1 end)
     header = "-EX0001-"
     IO.iodata_to_binary [header, Enum.take(random_number_stream, 20 - byte_size header)]
-  end
-
-  def add(<<"d", _::binary>> = raw) do
-    {:ok, data, info_hash} = Bencode.decode_with_info_hash(raw)
-    Torrent.Processes.add(info_hash, data)
   end
 end
