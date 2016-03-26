d3d8d8bd85c6d82eb9e107380527fbc24e387e7c
Martin Gausby <martin@gausby.dk>
2016-03-26 11:49:41 -0700

    Added module docs for the pieces supervisor

--- lib/torrent/file/pieces.ex
+++ lib/torrent/file/pieces.ex
@@ -1,6 +1,27 @@
 defmodule Torrent.File.Pieces do
   use Supervisor
 
+  @moduledoc """
+  Piece bookkeeping.
+
+  This is a supervisor that manage everything related to fetching
+  and verifying pieces in a torrent. It manage four processes:
+
+    - A controller, that other processes can give orders, such as
+      stating that they need a certain piece number
+    - A state agent that hold a bit-field representing the blocks
+      of the piece that has been fetched, and which ones are not
+      present yet
+    - A checksum agent that hold the checksums for the individual
+      pieces; when a peice has been requested a process responisble
+      for getting this piece will ask for the checksum here, which
+      it will use to verify the integrity of the downloaded piece
+      when complete
+    - A 'store' supervisor that spawn download processes of blocks
+      for a given piece. The name store should probably be
+      reconsidered at some point
+  """
+
   def start_link(info_hash, meta_info) do
     Supervisor.start_link(__MODULE__, {info_hash, meta_info})
   end
